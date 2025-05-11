81d84b33390489fbc61628f73b365798cee89479
Nhung Nguyen <nhungnguyen8404@gmail.com>
2025-05-12 04:48:30 +0700

    fix giao dien home + add data user

--- app/models/buyable_item.py
+++ app/models/buyable_item.py
@@ -6,6 +6,6 @@ class BuyableItem(csdl.Model):
     item_name = csdl.Column(csdl.String(100), nullable=False)
     price = csdl.Column(csdl.Integer)
     image = csdl.Column(csdl.String(100), nullable=True)
-    item_category_id = csdl.Column(csdl.Integer, csdl.ForeignKey('item_category.id'), nullable=False)
+    item_category_id = csdl.Column(csdl.Integer, csdl.ForeignKey('category.id'), nullable=False)
     status = csdl.Column(csdl.Integer, csdl.ForeignKey('buyable_item_status.id'), nullable=False)
     status_obj = csdl.relationship('BuyableItemStatus', backref='items', lazy=True)
